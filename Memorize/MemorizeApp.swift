4d5d277d1caf61a004adeecca4cead0b57a5bb05
Guilherme Magnabosco <guilherme.magnabosco@nave.rs>
2021-07-19 10:24:14 -0300

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by user on 19/07/21.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
